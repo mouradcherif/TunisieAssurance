3be41a60b8a3ca7d42e65f526aa9fec40306ca71
mouradcherif <105045769+mouradcherif@users.noreply.github.com>
2022-11-15 21:06:21 +0100

    Initial commit

---
+++ app/src/main/java/com/cherif/tunisieassurance/IntroActivity.java
@@ -0,0 +1,16 @@
+package com.cherif.tunisieassurance;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.motion.widget.MotionLayout;
+
+import android.os.Bundle;
+
+public class IntroActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        }
+
+
+}
---
+++ app/src/main/java/com/cherif/tunisieassurance/MainActivity.java
@@ -0,0 +1,37 @@
+package com.cherif.tunisieassurance;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.motion.widget.MotionLayout;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        MotionLayout motion_layout = findViewById(R.id.motion_layout);
+        motion_layout.setTransitionListener(new MotionLayout.TransitionListener() {
+            @Override
+            public void onTransitionStarted(MotionLayout motionLayout, int startId, int endId) {
+
+            }
+
+            @Override
+            public void onTransitionChange(MotionLayout motionLayout, int startId, int endId, float progress) {
+
+            }
+
+            @Override
+            public void onTransitionCompleted(MotionLayout motionLayout, int currentId) {
+
+            }
+
+            @Override
+            public void onTransitionTrigger(MotionLayout motionLayout, int triggerId, boolean positive, float progress) {
+
+            }
+        });
+    }
+}

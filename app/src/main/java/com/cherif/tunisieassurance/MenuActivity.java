276eb4770acd1bc832883eef99d81ccb0c9425b2
mouradcherif <105045769+mouradcherif@users.noreply.github.com>
2022-11-17 18:45:37 +0100

    bug fix

--- app/src/main/java/com/cherif/tunisieassurance/MenuActivity.java
+++ app/src/main/java/com/cherif/tunisieassurance/MenuActivity.java
@@ -1,5 +1,9 @@
 package com.cherif.tunisieassurance;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MenuActivity extends AppCompatActivity {
@@ -8,5 +12,25 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+
+    public void GoContracts(View view) {
+        Intent intent = new  Intent(getBaseContext(), ContractActivity.class);
+        startActivity(intent);
+    }
+
+    public void GoClients(View view) {
+        Intent intent = new  Intent(getBaseContext(), ClientActivity.class);
+        startActivity(intent);
+    }
+
+    public void GoGithub(View view) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/mouradcherif"));
+        startActivity(browserIntent);
+    }
+
+    public void GoSignIn(View view) {
+        Intent intent = new  Intent(getBaseContext(), LoginActivity.class);
+        startActivity(intent);
     }
 }

33a858725530307e16a836506f56516dbad847a8
mouradcherif <105045769+mouradcherif@users.noreply.github.com>
2022-11-19 00:47:59 +0100

    add new button function
    added new button function for new client with add edit client activity

--- app/src/main/java/com/cherif/tunisieassurance/ClientActivity.java
+++ app/src/main/java/com/cherif/tunisieassurance/ClientActivity.java
@@ -2,13 +2,32 @@ package com.cherif.tunisieassurance;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ClientActivity extends AppCompatActivity {
+
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client);
+
+        fab = findViewById(R.id.addclt);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ClientActivity.this,AddEditClient.class);
+                startActivity(intent);
+            }
+        });
     }
 }

1a88469054425f508a13c6d5495ec60e271dfdde
mouradcherif <105045769+mouradcherif@users.noreply.github.com>
2022-11-19 17:20:12 +0100

    getting values from ui

--- app/src/main/java/com/cherif/tunisieassurance/AddEditClient.java
+++ app/src/main/java/com/cherif/tunisieassurance/AddEditClient.java
@@ -1,14 +1,209 @@
 package com.cherif.tunisieassurance;
 
+import androidx.annotation.ContentView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageButton;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class AddEditClient extends AppCompatActivity {
+
+    private ImageView profileImage;
+    private EditText nameEt,phoneEt,emailEt,regionEt;
+    private AppCompatImageButton fab;
+    String name,phone,email,region;
+    ActionBar actionBar;
+    private static final int CAMERA_PERMISSION_CODE = 100;
+    private static final int STORAGE_PERMISSION_CODE = 200;
+    private static final int IMAGE_FROM_GALLERY_CODE = 300;
+    private static final int IMAGE_FROM_CAMERA_CODE = 100;
+    private String[] cameraPermission;
+    private String[] storagePermission;
+    Uri imageUri;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_edit_client);
+
+        cameraPermission = new String[]{Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        storagePermission = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+        actionBar = getSupportActionBar();
+
+        actionBar.setTitle("Add Contact");
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+
+        profileImage = findViewById(R.id.profile_image);
+        nameEt = findViewById(R.id.nameEt);
+        phoneEt = findViewById(R.id.phoneEt);
+        emailEt = findViewById(R.id.emailEt);
+        regionEt = findViewById(R.id.locationEt);
+        fab = findViewById(R.id.editinfo);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveData();
+            }
+        });
+
+        profileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showImagePickerDialog();
+            }
+        });
+    }
+
+    private void showImagePickerDialog() {
+        String options[] = {"Camera","Gallery"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Choose an option");
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                if (which == 0){
+                    if (!checkCameraPermission()){
+                        requestCameraPermission();
+                    }else{
+                        pickFromCamera();
+                    }
+
+                }else if (which == 1){
+                    if (!checkStoragePermission()){
+                        requestStoragePermission();
+                    }else {
+                        pickFromGallery();
+                    }
+                }
+            }
+        }).create().show();
+    }
+
+    private void pickFromGallery() {
+
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent,IMAGE_FROM_GALLERY_CODE);
+    }
+
+    private void pickFromCamera() {
+
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE,"IMAGE_TITLE");
+        values.put(MediaStore.Images.Media.DESCRIPTION,"IMAGE_DETAIL");
+
+        imageUri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,values);
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);
+
+        startActivityForResult(cameraIntent,IMAGE_FROM_CAMERA_CODE);
+    }
+
+    private void saveData() {
+
+        name = nameEt.getText().toString();
+        email = emailEt.getText().toString();
+        region = regionEt.getText().toString();
+        phone = phoneEt.getText().toString();
+
+        if (name.isEmpty() || phone.isEmpty() || email.isEmpty() || region.isEmpty()){
+
+            Toast.makeText(getApplicationContext(),"Please fill all fields", Toast.LENGTH_SHORT).show();
+
+        }else {
+
+        }
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return super.onSupportNavigateUp();
+    }
+
+    private boolean checkCameraPermission(){
+        boolean result = ContextCompat.checkSelfPermission(this,Manifest.permission.CAMERA) == (PackageManager.PERMISSION_GRANTED);
+        boolean result1 = ContextCompat.checkSelfPermission(this,Manifest.permission.WRITE_EXTERNAL_STORAGE) == (PackageManager.PERMISSION_GRANTED);
+        return result & result1;
+    }
+
+    private void requestCameraPermission(){
+        ActivityCompat.requestPermissions(this,cameraPermission,CAMERA_PERMISSION_CODE);
+    }
+
+    private boolean checkStoragePermission(){
+        boolean result1 = ContextCompat.checkSelfPermission(this,Manifest.permission.WRITE_EXTERNAL_STORAGE) == (PackageManager.PERMISSION_GRANTED);
+        return  result1;
+    }
+
+    private void requestStoragePermission(){
+        ActivityCompat.requestPermissions(this,storagePermission,CAMERA_PERMISSION_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case CAMERA_PERMISSION_CODE:
+                if (grantResults.length>0){
+
+                    boolean cameraAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    boolean storageAccepted = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+
+                    if (cameraAccepted && storageAccepted){
+                        pickFromCamera();
+                    }else {
+                        Toast.makeText(getApplicationContext(),"Camera & Storage Permission needed...", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                break;
+
+            case STORAGE_PERMISSION_CODE:
+                if (grantResults.length>0){
+
+
+                    boolean storageAccepted = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+
+                    if (storageAccepted){
+                        pickFromGallery();
+                    }else {
+                        Toast.makeText(getApplicationContext(),"Storage Permission needed...", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK){
+            profileImage.setImageURI(imageUri);
+        }
     }
 }
